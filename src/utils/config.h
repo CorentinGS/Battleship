2b8bad8aabdadc58679290ca58becce62259c5e6
CorentinGS <c.giaufersaubert@outlook.com>
2022-12-01 19:12:48 +0100

    :sparkles: Board & tile

--- src/utils/config.h
+++ src/utils/config.h
@@ -16,12 +16,12 @@
 #ifndef BATTLESHIP_CONFIG_H
 #define BATTLESHIP_CONFIG_H
 
-#define BOARD_WIDTH        10
-#define BOARD_HEIGHT       10
+#define BOARD_WIDTH        20
+#define BOARD_HEIGHT       20
 
-#define FRIGATE_COUNT      3
-#define DESTROYER_COUNT    2
-#define SHIP_CRUISER_COUNT 2
+#define FRIGATE_COUNT      1
+#define DESTROYER_COUNT    1
+#define SHIP_CRUISER_COUNT 1
 #define CARRIER_COUNT      1
 
 #endif
